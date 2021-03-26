ed1f0da139eb4e0b4e19afc54c1dbe37396d739e
Fauzan Ardiansyah <80576785+fauzanardiansyah@users.noreply.github.com>
2021-03-26 22:31:20 +0700

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\E learning\Semester 6\Modul 1\Mobile Programming\Software\flutter_windows_2.0.1-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\user\AndroidStudioProjects\flutter_firebase_auth_tutorial-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
